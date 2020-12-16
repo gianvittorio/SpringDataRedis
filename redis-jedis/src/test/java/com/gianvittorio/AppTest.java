b72b9562645ecbf1343949d3ef0711e0bccb83d3
gianvittorio <gianvittorio.castellano@fonyou.com>
2020-12-16 13:55:03 -0300

    first one

---
+++ redis-jedis/src/test/java/com/gianvittorio/AppTest.java
@@ -0,0 +1,21 @@
+package com.gianvittorio;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+/**
+ * Unit test for simple App.
+ */
+@ExtendWith(SpringExtension.class)
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
